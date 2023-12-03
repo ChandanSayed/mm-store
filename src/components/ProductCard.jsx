0f5a5bc7c05805d7146288fa2f9e8a4d2a4a3c03
Chandan Sayed <hangzhou635@gmail.com>
2023-12-04 04:04:01 +0600

    Product design upaded with product update url

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,8 +5,8 @@ const ProductCard = ({ product }) => {
   const { _id, image, name, brandName, type, price, shortDescription, rating } = product;
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-2xl bg-white">
-      <img src={image} alt={name} className="w-full h-64 object-cover object-center" />
+    <div className="max-w-sm rounded overflow-hidden shadow-2xl bg-white pt-5">
+      <img src={image} alt={name} className="w-full h-64 object-contain" />
       <div className="px-6 py-4">
         <div className="flex justify-between items-center">
           <div className="font-bold text-xl mb-2">{name}</div>
@@ -19,7 +19,9 @@ const ProductCard = ({ product }) => {
         <p className="text-gray-700 text-base mb-2">Rating: {rating}</p>
         <p className="text-gray-700 text-base mb-2">{shortDescription}</p>
         <div className="flex justify-between">
-          <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update</Link>
+          <Link to={`/products/product-update/${_id}`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Update
+          </Link>
           <Link to={`/products/product-details/${_id}`} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
             Details
           </Link>
