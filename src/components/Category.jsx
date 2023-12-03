7f71990d93054fbfd732c71c205ddffabf809036
Chandan Sayed <hangzhou635@gmail.com>
2023-12-04 03:36:00 +0600

    6 Categories are added

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,20 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Category = () => {
+  const brands = [
+    {
+      name: 'apple',
+      image: 'apple.png'
+    },
+    {
+      name: 'walton',
+      image: 'walton.png'
+    },
+    {
+      name: 'samsung',
+      image: 'samsung.jpg'
+    },
+    {
+      name: 'dell',
+      image: 'dell.webp'
+    },
+    {
+      name: 'hp',
+      image: 'hp.jpg'
+    },
+    {
+      name: 'accer',
+      image: 'accer.png'
+    }
+  ];
   return (
     <div className="px-4 my-12">
-      <h2>Available Brands</h2>
+      <h2 className="text-2xl lg:text-4xl mb-10 font-bold text-center">Available Brands</h2>
       <div className="max-w-[1440px] mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3">
-        <Link to={`/products/apple`}>
-          <div className="card w-96 bg-base-100 shadow-xl image-full">
-            <figure>
-              <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Shoes!</h2>
-            </div>
-          </div>
-        </Link>
+        {brands.map((brand, i) => {
+          return (
+            <Link to={`/products/${brand.name}`} key={i}>
+              <div className="card bg-base-100 shadow-xl image-full">
+                <figure>
+                  <img className="!w-full !h-[256px] lg:!h-[300px] !object-cover" src={`/images/${brand.image}`} alt={brand.name} />
+                </figure>
+                <div className="card-body items-center justify-center">
+                  <h2 className="card-title text-lg lg:text-3xl capitalize">{brand.name}</h2>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
