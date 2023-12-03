d9461184388b0fd2da8b88f3952ba4333a92e70c
Chandan Sayed <hangzhou635@gmail.com>
2023-12-04 03:55:16 +0600

    Products unavailable design added

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -28,12 +28,23 @@ const CategoryProducts = () => {
   console.log(name);
   return (
     <div className="bg-base-200">
-      <Slider />
-      <div className="max-w-[1440px] px-4 mx-auto py-12">
-        {products.map(product => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-      </div>
+      {products.length > 0 ? (
+        <>
+          <Slider />
+          <div className="max-w-[1440px] px-4 mx-auto py-12">
+            {products.map(product => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="text-center px-4">
+            <h2 className="text-2xl lg:text-4xl font-bold mb-6">No products available in this category right now!</h2>
+            <p>We are trying out best to add products to this category. Thank you for visiting!</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
